10ea8dd92fbdcdaf175493795561c9158d0cb55a
JoaoPaulo-LD <joaopauloalencarrodrigues@gmail.com>
2020-10-12 14:01:29 -0300

    OrphanagesMap page finished

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 import { Link } from 'react-router-dom'
 import { FiPlus } from 'react-icons/fi'
+import { Map, TileLayer } from 'react-leaflet'
+
+import 'leaflet/dist/leaflet.css'
 
 import mapMarkerimg from '../images/map-marker.svg'
 
@@ -24,7 +27,17 @@ const OrphanagesMap: React.FC = () => {
         </footer>
       </aside>
 
-      <div></div>
+      <Map
+        center={[-2.4384375,-54.7176032]}
+        zoom={15.12}
+        style={{ width: '100%', height: '100%' }}
+      >
+        <TileLayer
+          url={
+            `https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`
+          }
+        />
+      </Map>
 
       <Link to="" className="create-orphanage">
         <FiPlus size={32} color="white" />
