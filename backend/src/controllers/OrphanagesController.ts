e7c80d8c7b610b3f388fd363c6cbb5bf2f059bf0
JoaoPaulo-LD <joaopauloalencarrodrigues@gmail.com>
2020-10-13 15:35:07 -0300

    Back-end finished

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express'
 import { getRepository } from 'typeorm'
 import Orphanage from '../models/Orphanage'
+import orphanageView from '../views/orphanages_view'
+import * as Yup from 'yup'
 
 import '../database/connection'
 
@@ -25,32 +27,51 @@ export default {
       return { path: image.filename }
     })
 
-    try {
-      const orphanage = orphanagesRepository.create({
-        name,
-        latitude,
-        longitude,
-        about,
-        instructions,
-        opening_hours,
-        open_on_weekends,
-        images
-      })
-
-      await orphanagesRepository.save(orphanage)
-
-      return res.status(201).json(orphanage)
-    } catch (e) {
-      console.log(e)
+    const data = {
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends,
+      images
     }
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      latitude: Yup.number().required(),
+      longitude: Yup.number().required(),
+      about: Yup.string().required().max(300),
+      instructions: Yup.string().required(),
+      opening_hours: Yup.string().required(),
+      open_on_weekends: Yup.boolean().required(),
+      images: Yup.array(
+        Yup.object().shape({
+          path: Yup.string().required()
+        })
+      )
+    })
+
+    await schema.validate(data, {
+      abortEarly: false
+    })
+
+    const orphanage = orphanagesRepository.create(data)
+
+    await orphanagesRepository.save(orphanage)
+
+    return res.status(201).json(orphanage)
   },
 
   async index(req: Request, res: Response) {
     const orphanagesRepository = getRepository(Orphanage)
 
-    const orphanages = await orphanagesRepository.find()
+    const orphanages = await orphanagesRepository.find({
+      relations: ['images']
+    })
 
-    return res.status(202).json(orphanages)
+    return res.status(202).json(orphanageView.renderMany(orphanages))
   },
 
   async show(req: Request, res: Response) {
@@ -58,8 +79,10 @@ export default {
 
     const orphanagesRepository = getRepository(Orphanage)
 
-    const orphanage = await orphanagesRepository.findOneOrFail(id)
+    const orphanage = await orphanagesRepository.findOneOrFail(id, {
+      relations: ['images']
+    })
 
-    return res.status(202).json(orphanage)
+    return res.status(202).json(orphanageView.render(orphanage))
   }
 }
