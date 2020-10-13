e94238dbe983d853a8db20ac98fe293cb20473e0
JoaoPaulo-LD <joaopauloalencarrodrigues@gmail.com>
2020-10-13 11:38:49 -0300

    Added main Orphanages routes

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express'
 import { getRepository } from 'typeorm'
 import Orphanage from '../models/Orphanages'
 
-import './database/connection'
+import '../database/connection'
 
 export default {
   async create(req: Request, res: Response) {
@@ -31,5 +31,23 @@ export default {
     await orphanagesRepository.save(orphanage)
 
     return res.status(201).json(orphanage)
+  },
+
+  async index(req: Request, res: Response) {
+    const orphanagesRepository = getRepository(Orphanage)
+
+    const orphanages = await orphanagesRepository.find()
+
+    return res.status(202).json(orphanages)
+  },
+
+  async show(req: Request, res: Response) {
+    const { id } = req.params
+
+    const orphanagesRepository = getRepository(Orphanage)
+
+    const orphanage = await orphanagesRepository.findOneOrFail(id)
+
+    return res.status(202).json(orphanage)
   }
 }
